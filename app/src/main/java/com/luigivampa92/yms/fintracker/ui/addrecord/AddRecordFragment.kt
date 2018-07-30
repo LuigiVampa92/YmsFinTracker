92500400aa60cd86bf7639bafa204a1dd04a454a
Pavel Vasiliev <luigivampa92@gmail.com>
2018-07-30 04:13:40 +0300

    VASILIEV-2 Добавил дейтпикер в окно добавления записи

--- app/src/main/java/com/luigivampa92/yms/fintracker/ui/addrecord/AddRecordFragment.kt
+++ app/src/main/java/com/luigivampa92/yms/fintracker/ui/addrecord/AddRecordFragment.kt
@@ -8,9 +8,13 @@ import android.view.ViewGroup
 import android.widget.EditText
 import butterknife.BindView
 import butterknife.ButterKnife
+import butterknife.OnClick
 import butterknife.Unbinder
 import com.luigivampa92.yms.fintracker.R
 import com.luigivampa92.yms.fintracker.ui.base.BaseFragment
+import android.app.DatePickerDialog
+import java.text.SimpleDateFormat
+import java.util.*
 
 class AddRecordFragment : BaseFragment() {
 
@@ -27,12 +31,18 @@ class AddRecordFragment : BaseFragment() {
     protected lateinit var toolbar: Toolbar
     @BindView(R.id.edit_text_amount)
     protected lateinit var editTextAmount: EditText
+    @BindView(R.id.edit_text_date)
+    protected lateinit var editTextDate: EditText
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?) =
             inflater.inflate(R.layout.fragment_add_record, container, false).also {
                 unbinder = ButterKnife.bind(this, it)
                 setupToolbar()
+
                 editTextAmount.setText("0")
+
+                val formatted = SimpleDateFormat.getDateInstance().format(Date())
+                editTextDate.setText(formatted)
             }
 
     override fun onDestroyView() {
@@ -54,5 +64,22 @@ class AddRecordFragment : BaseFragment() {
         addRecordActivity.supportActionBar?.setDisplayShowHomeEnabled(true)
         addRecordActivity.supportActionBar?.setHomeButtonEnabled(true)
         addRecordActivity.supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    @OnClick(R.id.edit_text_date)
+    protected fun editTextDateClicked() {
+        val calendar = Calendar.getInstance()
+
+        val dateListener = DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+            calendar.set(Calendar.YEAR, year)
+            calendar.set(Calendar.MONTH, monthOfYear)
+            calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+
+            val date = calendar.time
+            val formatted = SimpleDateFormat.getDateInstance().format(date)
+            editTextDate.setText(formatted)
+        }
+
+        DatePickerDialog(activity, R.style.DatePickerTheme, dateListener, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH)).show()
     }
 }
