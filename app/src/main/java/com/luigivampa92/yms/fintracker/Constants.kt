0194bfa2fe0d77ef6d268290b21c298878c64bfe
Vladimir Yugay <voviktyl@gmail.com>
2018-08-05 21:47:28 +0300

    Добавлена периодчиность.

--- app/src/main/java/com/luigivampa92/yms/fintracker/Constants.kt
+++ app/src/main/java/com/luigivampa92/yms/fintracker/Constants.kt
@@ -8,4 +8,16 @@ object Constants{
     const val CURRENT_WALLET_BALANCE = "CURRENT_WALLET_BALANCE"
     const val ID = "ID"
     const val SECONDARY_CURRENCY = "SECONDARY_CURRENCY"
+    const val REPEATED_JOB_ID = 888
+    const val JOB_ID = 777
+
+    const val RECORD_NAME = "RECORD_NAME"
+    const val RECORD_CATEGORY = "RECORD_CATEGORY"
+    const val RECORD_INCOME = "RECORD_INCOME"
+    const val RECORD_AMOUNT = "RECORD_AMOUNT"
+    const val RECORD_CURRENCY = "RECORD_CURRENCY"
+    const val RECORD_WALET_ID = "RECORD_WALET_ID"
+    const val RECORD_DATE = "RECORD_DATE"
+    const val RECORD_PENDING_DATE = "RECORD_PENDING_DATE"
+    const val WALLET_BALANCE = "WALLET_BALANCE"
 }
--- app/src/main/java/com/luigivampa92/yms/fintracker/view/activities/ActivityAddRecord.kt
+++ app/src/main/java/com/luigivampa92/yms/fintracker/view/activities/ActivityAddRecord.kt
@@ -1,10 +1,14 @@
 package com.luigivampa92.yms.fintracker.view.activities
 
 import android.app.DatePickerDialog
+import android.app.job.JobInfo
+import android.app.job.JobScheduler
 import android.arch.lifecycle.ViewModelProviders
+import android.content.ComponentName
 import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import java.lang.Double.parseDouble
 import android.widget.ArrayAdapter
 import com.luigivampa92.yms.fintracker.Constants
@@ -13,6 +17,8 @@ import com.luigivampa92.yms.fintracker.calculations.CurrencyConverter
 import com.luigivampa92.yms.fintracker.utils.getTextFromView
 import com.luigivampa92.yms.fintracker.utils.hasText
 import com.luigivampa92.yms.fintracker.model.Record
+import com.luigivampa92.yms.fintracker.scheduler.JobSchedulerService
+import com.luigivampa92.yms.fintracker.utils.createId
 import com.luigivampa92.yms.fintracker.viewmodel.ViewModelAddRecord
 import kotlinx.android.synthetic.main.activity_add_record.*
 import java.util.*
@@ -71,17 +77,6 @@ class ActivityAddRecord : AppCompatActivity() {
             datePickerDialog.show()
         }
 
-        pending_date_activity_add_record.setOnClickListener {
-            val calendar = Calendar.getInstance()
-            val datePickerDialog = DatePickerDialog(this,
-                    R.style.MateriaCalendar,
-                    DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
-                        pending_date_label_activity_add_record.text = this.resources.getString(R.string.formatted_date, dayOfMonth, month, year)
-                    },
-                    calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH),
-                    calendar.get(Calendar.DAY_OF_WEEK_IN_MONTH))
-            datePickerDialog.show()
-        }
 
         done_activity_add_record.setOnClickListener { _ ->
             if (hasText(name_activity_add_record, category_activity_add_record,
@@ -94,7 +89,7 @@ class ActivityAddRecord : AppCompatActivity() {
                 val currency = currency_activity_add_record.selectedItem.toString()
                 val walletId = sf.getString(Constants.CURRENT_WALLET_ID, "DEFAULT")
                 val date = getTextFromView(date_activity_add_record)
-                val pendingDate = getTextFromView(pending_date_activity_add_record)
+                val pendingDate = getTextFromView(pending_activity_add_record)
 
                 if (!income) amount = -amount
 
@@ -107,13 +102,44 @@ class ActivityAddRecord : AppCompatActivity() {
                         walletId,
                         date,
                         pendingDate)
-
-                mViewModel.addRecord(record)
-                //Перед обновлением баланса выбранного кошелька, переводим "ценность" записи в $
                 val walletBalance = parseDouble(sf.getString(Constants.CURRENT_WALLET_BALANCE, null)) +
                         CurrencyConverter.convertCurrency(currency, amount, "USD")
-                mViewModel.updateWallet(walletId, walletBalance)
-                sf.edit().putString(Constants.CURRENT_WALLET_BALANCE, walletBalance.toString()).apply()
+
+                val bundle = PersistableBundle()
+                bundle.putString(Constants.RECORD_NAME, name)
+                bundle.putString(Constants.RECORD_CATEGORY, category)
+                bundle.putInt(Constants.RECORD_INCOME, if (income) 1 else 0)
+                bundle.putDouble(Constants.RECORD_AMOUNT, amount)
+                bundle.putString(Constants.RECORD_CURRENCY, currency)
+                bundle.putString(Constants.RECORD_WALET_ID, walletId)
+                bundle.putString(Constants.RECORD_DATE, date)
+                bundle.putDouble(Constants.WALLET_BALANCE, walletBalance)
+
+                if (hasText(pending_activity_add_record) && repeat_activity_add_record.isChecked) {
+                    val service = ComponentName(this, JobSchedulerService::class.java)
+                    val jobInfo = JobInfo.Builder(Constants.REPEATED_JOB_ID, service)
+                            .setPeriodic(pending_activity_add_record.text.toString().toLong() * 864 * 100000)
+                            .setPersisted(true)
+                            .setExtras(bundle)
+                            .build()
+                    val scheduler = this.getSystemService(Context.JOB_SCHEDULER_SERVICE) as JobScheduler
+                    scheduler.schedule(jobInfo)
+                } else if (hasText(pending_activity_add_record) && !repeat_activity_add_record.isChecked) {
+                    val service = ComponentName(this, JobSchedulerService::class.java)
+                    val jobInfo = JobInfo.Builder(Constants.JOB_ID, service)
+                            .setMinimumLatency(getTextFromView(pending_activity_add_record).toLong() * 864 * 100000)
+                            .setPersisted(true)
+                            .setExtras(bundle)
+                            .build()
+                    val scheduler = this.getSystemService(Context.JOB_SCHEDULER_SERVICE) as JobScheduler
+                    scheduler.schedule(jobInfo)
+                } else {
+                    mViewModel.addRecord(record)
+                    //Перед обновлением баланса выбранного кошелька, переводим "ценность" записи в $
+                    mViewModel.updateWallet(walletId, walletBalance)
+                    sf.edit().putString(Constants.CURRENT_WALLET_BALANCE, walletBalance.toString()).apply()
+                }
+
                 finish()
             }
         }
