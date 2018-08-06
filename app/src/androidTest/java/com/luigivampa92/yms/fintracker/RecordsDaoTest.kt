248bbcb65814a23ebd4d1f64a1e7da9c96a65000
Vladimir Yugay <voviktyl@gmail.com>
2018-08-06 07:25:33 +0300

    Добавлены инструментальные тесты для RecordsDao

--- app/src/androidTest/java/com/luigivampa92/yms/fintracker/RecordsDaoTest.kt
+++ app/src/androidTest/java/com/luigivampa92/yms/fintracker/RecordsDaoTest.kt
@@ -2,46 +2,36 @@ package com.luigivampa92.yms.fintracker
 
 import android.arch.lifecycle.LiveData
 import android.arch.lifecycle.Observer
-import android.arch.persistence.room.Room
-import android.support.test.InstrumentationRegistry
 import android.support.test.runner.AndroidJUnit4
-import com.luigivampa92.yms.fintracker.db.dao.WalletsDao
-import com.luigivampa92.yms.fintracker.db.database.FinanceTrackerDatabase
+import com.luigivampa92.yms.fintracker.model.Record
 import com.luigivampa92.yms.fintracker.model.Wallet
 import com.luigivampa92.yms.fintracker.utils.createId
-import org.junit.After
 import org.junit.Assert
-import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import java.util.concurrent.CountDownLatch
 import java.util.concurrent.TimeUnit
 
 @RunWith(AndroidJUnit4::class)
-class WalletsDaoTest : DbTest() {
+class RecordsDaoTest: DbTest() {
 
     val walletId = createId()
-    val wallet = Wallet(walletId, "Wallet", 150.0, "USD")
+    val record = Record(0L, "Burger", "Food", false, 120.0,
+            "RUB", walletId, "06.08.2018", null)
 
     @Test
-    fun shouldDeleteAllData() {
-        db.walletsDao().addWallet(wallet)
-        db.walletsDao().deleteAllWallets()
-        Assert.assertEquals(getValue(db.walletsDao().getAllWallets()).size, 0)
+    fun shouldDeleteAllData(){
+        db.recordsDao().addRecord(record)
+        db.recordsDao().deleteAllRecords()
+        Assert.assertEquals(getValue(db.recordsDao().getAllRecordsFromWallet(walletId)).size, 0)
     }
 
     @Test
-    fun shouldAddData() {
-        db.walletsDao().addWallet(wallet)
-        Assert.assertEquals(getValue(db.walletsDao().getWallet(walletId)).name, wallet.name)
+    fun shouldAddData(){
+        db.recordsDao().addRecord(record)
+        Assert.assertEquals(getValue(db.recordsDao().getAllRecordsFromWallet(walletId)).first().name, record.name)
     }
 
-    @Test
-    fun shouldUpdateData() {
-        db.walletsDao().addWallet(wallet)
-        db.walletsDao().updateWalletBalance(walletId, 20.0)
-        Assert.assertEquals(getValue(db.walletsDao().getWallet(walletId)).balance, 20.0, 0.002)
-    }
 
     @Throws(InterruptedException::class)
     fun <T> getValue(liveData: LiveData<T>): T {
